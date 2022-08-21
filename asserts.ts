10332960ef75dd189bd5d44f9fb50df29520fddc
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-08-21 17:50:02 +0900

    feat(asserts): add JavaScript data type assertion

--- asserts.ts
+++ asserts.ts
@@ -1,4 +1,20 @@
+import {
+  isBoolean,
+  isFunction,
+  isNull,
+  isNumber,
+  isString,
+  isSymbol,
+  isUndefined,
+} from "./deps.ts";
 import { FailResult, Result, Schema, SuccessResult } from "./types.ts";
+import {
+  createAssertFromTypeGuard,
+  createSchemaErrorThrower,
+  inspect,
+} from "./utils.ts";
+import { AssertionError, SchemaError } from "./errors.ts";
+import { isObject } from "https://deno.land/x/isx@1.0.0-beta.19/mod.ts";
 
 /** Assert whether the value satisfies the schema.
  *
@@ -41,4 +57,93 @@ export function isSuccessResult<T>(
 
 export function isFailResult<T>(result: Result<T>): result is FailResult {
   return "errors" in result;
+}
+
+export function isSchema(value: unknown): value is Schema {
+  return isFunction(Object(value)["validate"]);
+}
+
+export function isSchemaError(value: unknown): value is SchemaError {
+  return value instanceof SchemaError;
+}
+
+export function isAssertionError(value: unknown): value is AssertionError {
+  return value instanceof AssertionError;
+}
+
+export function assertString(value: unknown): asserts value is string {
+  createAssertFromTypeGuard(
+    isString,
+    createSchemaErrorThrower("string"),
+  ).call(null, value);
+}
+
+export function assertBoolean(value: unknown): asserts value is boolean {
+  createAssertFromTypeGuard(
+    isBoolean,
+    createSchemaErrorThrower("boolean"),
+  ).call(null, value);
+}
+
+export function assertNumber(value: unknown): asserts value is number {
+  createAssertFromTypeGuard(
+    isNumber,
+    createSchemaErrorThrower("number"),
+  ).call(null, value);
+}
+
+function isBigint(value: unknown): value is bigint {
+  return typeof value === "bigint";
+}
+
+export function assertBigint(value: unknown): asserts value is bigint {
+  const _ = createAssertFromTypeGuard(
+    isBigint,
+    createSchemaErrorThrower("number"),
+  )(value);
+}
+
+export function assertNull(value: unknown): asserts value is null {
+  createAssertFromTypeGuard(
+    isNull,
+    createSchemaErrorThrower("null"),
+  ).call(null, value);
+}
+
+export function assertUndefined(value: unknown): asserts value is undefined {
+  createAssertFromTypeGuard(
+    isUndefined,
+    createSchemaErrorThrower("undefined"),
+  ).call(null, value);
+}
+
+export function assertSymbol(value: unknown): asserts value is symbol {
+  createAssertFromTypeGuard(
+    isSymbol,
+    createSchemaErrorThrower("symbol"),
+  ).call(null, value);
+}
+
+export function assertFunction(value: unknown): asserts value is Function {
+  createAssertFromTypeGuard(
+    isFunction,
+    createSchemaErrorThrower("function"),
+  ).call(null, value);
+}
+
+export function assertObject(value: unknown): asserts value is object {
+  createAssertFromTypeGuard(
+    isObject,
+    createSchemaErrorThrower("object"),
+  ).call(null, value);
+}
+
+export function assertIs<T>(base: T, value: unknown): asserts value is T {
+  const valid = Object.is(base, value);
+
+  if (!valid) {
+    throw new AssertionError(
+      `Not equal: ${inspect(base)} <- ${inspect(value)}`,
+    );
+  }
 }
