0844ef33b841c15f9d4977c03ca29696124555c8
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-08-21 22:36:20 +0900

    feat(schemas): add single field check to array schema

--- schemas/built_in.ts
+++ schemas/built_in.ts
@@ -1,6 +1,9 @@
-import { SchemaImpl } from "./types.ts";
+import { SchemaImpl, Unwrap } from "./types.ts";
+import { Schema } from "../types.ts";
 import { DataFlow } from "../utils.ts";
 import { assertArray, assertObject } from "../asserts.ts";
+import { isFailResult } from "../type_guards.ts";
+import { SchemaError } from "../errors.ts";
 
 /** Schema definition of built-in `Array`.
  *
@@ -10,8 +13,33 @@ import { assertArray, assertObject } from "../asserts.ts";
  * const value: unknown = undefined;
  * assertSchema(new ArraySchema(), value);
  * // value is `any[]`
+ * assertSchema(new ArraySchema(new StringSchema()), value);
+ * // value is `string[]`
  * ```
  */
-export class ArraySchema extends SchemaImpl<any[]> {
-  protected override dataFlow = new DataFlow(assertObject).define(assertArray);
+export class ArraySchema<T extends Schema | undefined = undefined>
+  extends SchemaImpl<T extends Schema ? Unwrap<T[]> : any[]> {
+  constructor(subType?: T) {
+    super();
+
+    if (subType) {
+      this.dataFlow = new DataFlow(assertObject).define(assertArray).define(
+        (value) => {
+          for (const [index, v] of value.entries()) {
+            const result = subType.validate(v);
+
+            if (isFailResult(result)) {
+              throw new SchemaError(`Invalid field. $[${index}]`, {
+                children: result.errors,
+              });
+            }
+          }
+        },
+      );
+    }
+  }
+
+  protected override dataFlow = new DataFlow(assertObject).define(
+    assertArray,
+  ) as DataFlow<any>;
 }
