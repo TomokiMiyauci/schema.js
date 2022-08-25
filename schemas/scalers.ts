2d57a799d5dd2ac2d5f765d53ef9584739ad0094
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-08-25 23:08:49 +0900

    feat(utils): improve error message

--- schemas/scalers.ts
+++ schemas/scalers.ts
@@ -11,7 +11,7 @@ import {
   assertUndefined,
   isUndefined,
 } from "../deps.ts";
-import { DataFlow, rethrow, schemaErrorThrower } from "../utils.ts";
+import { DataFlow } from "../utils.ts";
 import { Schema } from "../types.ts";
 
 /** Schema definition of `boolean`. */
@@ -29,7 +29,7 @@ export class BooleanSchema<T extends boolean>
       this.assertion = assertBoolean;
     } else {
       this.assertion = new DataFlow(assertBoolean).and(
-        rethrow(arity(assertIs, subType), schemaErrorThrower),
+        arity(assertIs, subType),
       ).build();
     }
   }
@@ -47,7 +47,7 @@ export class StringSchema<T extends string>
       this.assertion = assertString;
     } else {
       this.assertion = new DataFlow(assertString).and(
-        rethrow(arity(assertIs, subType), schemaErrorThrower),
+        arity(assertIs, subType),
       ).build();
     }
   }
@@ -66,7 +66,7 @@ export class NumberSchema<T extends number = number>
       this.assertion = assertNumber;
     } else {
       this.assertion = new DataFlow(assertNumber).and(
-        rethrow(arity(assertIs, subType), schemaErrorThrower),
+        arity(assertIs, subType),
       ).build();
     }
   }
@@ -85,7 +85,7 @@ export class BigintSchema<T extends bigint = bigint>
       this.assertion = assertBigint;
     } else {
       this.assertion = new DataFlow(assertBigint).and(
-        rethrow(arity(assertIs, subType), schemaErrorThrower),
+        arity(assertIs, subType),
       ).build();
     }
   }
--- utils.ts
+++ utils.ts
@@ -1,35 +1,6 @@
-import { Assert, inspect, TypeGuard } from "./deps.ts";
+import { Assert, AssertionError } from "./deps.ts";
 import { SchemaError } from "./errors.ts";
-import { TypeStr } from "./types.ts";
 import { isSchemaError } from "./type_guards.ts";
-
-export function createSchemaErrorThrower(
-  type: TypeStr,
-): (value: unknown) => never {
-  return (value) => {
-    const actual = typeof value;
-
-    throw new SchemaError(
-      `Invalid type. ${inspect(type)} <- ${inspect(actual)}`,
-      {
-        actual,
-        expected: type,
-      },
-    );
-  };
-}
-
-export function createAssertFromTypeGuard<V, R extends V>(
-  typeGuard: TypeGuard<V, R>,
-  onFail: (value: V) => never,
-): Assert<V, R> {
-  return (value) => {
-    const result = typeGuard(value);
-    if (!result) {
-      onFail(value);
-    }
-  };
-}
 
 export class DataFlow<In = unknown, Out extends In = In> {
   assertions: Assert<In, Out>[] = [];
@@ -54,26 +25,13 @@ export class DataFlow<In = unknown, Out extends In = In> {
 export function toSchemaError(e: unknown): SchemaError {
   if (isSchemaError(e)) return e;
 
+  if (e instanceof AssertionError) {
+    return new SchemaError(e.message);
+  }
+
   if (e instanceof Error) {
     return new SchemaError(`${e.name} has occurred.`, { cause: e });
   }
 
   return new SchemaError(`Unknown error has occurred.`);
-}
-
-export function schemaErrorThrower(value: unknown): never {
-  throw toSchemaError(value);
-}
-
-export function rethrow<F extends (value: unknown) => never | void>(
-  fn: F,
-  thrower: (e: unknown) => never,
-): (...args: Parameters<F>) => void | never {
-  return (...args) => {
-    try {
-      fn.apply(null, args);
-    } catch (e) {
-      thrower(e);
-    }
-  };
 }
