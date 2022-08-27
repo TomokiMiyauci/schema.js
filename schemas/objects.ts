738077187ebd73a22fd7d9bbca06e862b0ecdb10
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-08-27 11:19:40 +0900

    refactor(schemas): move built-in objects schema to objects file

--- schemas/objects.ts
+++ schemas/objects.ts
@@ -3,14 +3,16 @@ import { Schema, UnwrapSchema } from "../types.ts";
 import { SchemaError } from "../errors.ts";
 import {
   arity,
+  assertArray,
+  assertDate,
   assertFunction,
   assertObject,
   assertSameCount,
   inspect,
   isUndefined,
 } from "../deps.ts";
-import { assertProperty } from "../asserts.ts";
-import { DataFlow, toSchemaError } from "../utils.ts";
+import { assertOr, assertProperty } from "../asserts.ts";
+import { DataFlow, toSchema, toSchemaError } from "../utils.ts";
 
 /** Schema definition of `object`. */
 export class ObjectSchema<T extends object> extends CollectiveTypeSchema<
@@ -71,4 +73,66 @@ export class TupleSchema<T extends Schema[]>
   };
 
   protected override create = () => new TupleSchema(...this.#subType);
+}
+
+/** Schema definition of built-in `Array`.
+ *
+ * ```ts
+ * import { ArraySchema, assertSchema, StringSchema } from "https://deno.land/x/schema_js@$VERSION/mod.ts";
+ *
+ * const value: unknown = undefined;
+ * assertSchema(new ArraySchema(), value);
+ * // value is `{}[]`
+ * assertSchema(new ArraySchema([new StringSchema()]), value);
+ * // value is `string[]`
+ * ```
+ */
+export class ArraySchema<T>
+  extends CollectiveTypeSchema<unknown, UnwrapSchema<T>[]> {
+  protected override assertion: (
+    value: unknown,
+  ) => asserts value is UnwrapSchema<T[]>;
+
+  constructor(private subType?: readonly T[]) {
+    super();
+
+    if (isUndefined(this.subType)) {
+      this.assertion = assertArray;
+    } else {
+      const subType = this.subType;
+      this.assertion = new DataFlow().and(assertArray).and(
+        (value) => {
+          value.forEach((v) => {
+            const assertions = subType.map((v) => toSchema(v).assert);
+            assertOr(assertions, v);
+          });
+        },
+      ).build();
+    }
+  }
+
+  protected override create = () => new ArraySchema(this.subType);
+}
+
+/** Schema of `Date` object.
+ *
+ * ```ts
+ * import {
+ *   assertSchema,
+ *   DateSchema,
+ * } from "https://deno.land/x/schema_js@$VERSION/mod.ts";
+ * import {
+ *   assertThrows,
+ * } from "https://deno.land/std@$VERSION/testing/asserts.ts";
+ *
+ * const schema = new DateSchema();
+ * assertSchema(schema, new Date());
+ * assertThrows(() => assertSchema(schema, {}));
+ * ```
+ */
+export class DateSchema extends CollectiveTypeSchema<object, Date> {
+  protected override assertion: (value: object) => asserts value is Date =
+    assertDate;
+
+  protected override create = () => new DateSchema();
 }
